d62f88a277691f4dd195a13f6919e8717bdbcf0e
40371231H <40371231H@live.ntnu.edu.tw>
2017-05-10 14:58:10 +0800

    Start up

---
+++ da_fu_won/www/scripts/dice.js
@@ -0,0 +1,19 @@
+﻿function dice() {
+    var w = 0;
+    //i=parseInt(document.getElementById("inp").num.value);
+    //i=eval(document.getElementById("inp").num.value);
+    w = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("ans").innerHTML = "擲出 " + w + " 點";
+    alert("擲出 " + w + " 點");
+
+    if (w == 1) {
+        window.location = "choice.html";
+    } else if (w == 2) {
+        window.location = "fillin.html";
+    } else if (w == 3) {
+        window.location = "speak.html";
+    } else if (w == 4) {
+        window.location = "";
+    }
+}
+
